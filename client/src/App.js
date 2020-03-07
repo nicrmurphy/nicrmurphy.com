423e4eefc5eda465c8f665f7e20b9c103fa779d5
Nic Murphy <nicrmurphy@live.com>
2020-03-06 23:50:47 -0600

    project setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { Container, Typography } from '@material-ui/core'
+
+function App() {
+  return (
+    <Container className="App">
+      <Typography className="App-header" variant="h1">COMING SOON</Typography>
+    </Container>
+  );
+}
+
+export default App
